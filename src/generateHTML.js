5bf610073a80b7729f9814cb723254b34fdbf88e
dannikaml <dannikaml@hotmail.com>
2023-02-25 22:48:58 -0600

    fix css/bootstrap

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,12 +1,12 @@
 const generateManager = function (manager) {
     return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
+        <div class="card h-100 p-3 mb-2 bg-info text-white">
+            <div class="card-header p-3 mb-2 bg-secondary text-white">
                 <h3>${manager.name}</h3>
                 <h4>Manager</h4><i class="material-icons" style="font-size:15px;color:red;">cloud</i>
             </div>
-            <div class="card-body">
+            <div class="card-body p-3 mb-2 bg-light text-dark">
                 <p class="id">ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                 <p class="office">Office Number: ${manager.officeNumber}</p>
@@ -20,12 +20,12 @@ const generateManager = function (manager) {
 const generateEngineer = function (engineer) {
     return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
+        <div class="card h-100 p-3 mb-2 bg-info text-white">
+            <div class="card-header p-3 mb-2 bg-secondary text-white">
                 <h3>${engineer.name}</h3>
                 <h4>Engineer</h4><i class="material-icons">computer</i>
             </div>
-            <div class="card-body">
+            <div class="card-body p-3 mb-2 bg-light text-dark">
                 <p class="id">ID: ${engineer.id}</p>
                 <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
                 <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
@@ -39,12 +39,12 @@ const generateEngineer = function (engineer) {
 const generateIntern = function (intern) {
     return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
+        <div class="card h-100 p-3 mb-2 bg-info text-white">
+            <div class="card-header p-3 mb-2 bg-secondary text-white">
                 <h3>${intern.name}</h3>
                 <h4>Intern</h4><i class="material-icons">attachment</i>
             </div>
-            <div class="card-body">
+            <div class="card-body p-3 mb-2 bg-light text-dark">
                 <p class="id">ID: ${intern.id}</p>
                 <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
                 <p class="school">School: ${intern.school}</p>
